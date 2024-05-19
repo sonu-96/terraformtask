4f86e08f485e90eb09ba3035a08199e4fca3ae6b
sonu yadav <155170525+sonu-96@users.noreply.github.com>
2024-05-19 07:19:44 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -8,27 +8,56 @@ resource "aws_ecs_cluster" "terracluster" {
   name = "terracluster"
 }
 
-
 resource "aws_ecs_task_definition" "mytask_terra_definition" {
   family = "mytask"
+  requires_compatibilities = ["FARGATE"]
+  network_mode = "awsvpc"  # Required for Fargate
+  cpu = 256
+  memory = 512
   container_definitions = jsonencode([{
     name = "terra-container"
-    image = "docker.io/mdhack/myapache"
+    image = "docker.io/sonu96/nodejs"
     cpu = 256
     memory = 512
-    portMapping = [{
+    essential = true
+    portMappings = [
+      {
         containerPort = 80
-        hostPort = 80
-    }] 
+      }
+    ]
   }])
 }
+
 
 resource "aws_ecs_service" "terra_service" {
   name = "terra_service"
   cluster = aws_ecs_cluster.terracluster.id
   task_definition = aws_ecs_task_definition.mytask_terra_definition.arn
   desired_count = 1
+  launch_type = "FARGATE"
 }
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 
 resource "aws_lb" "my-lb" {
   name = "my-lb"
